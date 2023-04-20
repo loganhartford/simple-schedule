6bfffcbfa386016168fc1564470a198c0027dccb
loganhartford <56402946+loganhartford@users.noreply.github.com>
2023-04-20 13:27:31 -0500

    added the fix for safari flexbox gap

--- src/js/controller.js
+++ src/js/controller.js
@@ -106,10 +106,28 @@ const init = function () {
 
 init();
 
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
 /*
 Next Steps:
-- try and figure out a non-cancer way to center the schedule properly
 - make the website responsive
-- Add favicon and meta description
 - add browser support for flexbox in safari (css course)
 */
